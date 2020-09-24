dbb32168ab3e2dd920ea54d1b4afd0df0f1cebc6
Gilad <gilad.frish@gmail.com>
2020-09-24 19:01:39 +0300

    updated scrape_top_in_category - top 10 players in category

--- scrape_top_in_category.py
+++ scrape_top_in_category.py
@@ -1,11 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
+from team_roster import *
 
-source = requests.get('https://sports.yahoo.com/nba/stats/individual/?sortStatId=TOTAL_REBOUNDS_PER_GAME&selectedTable=0/').text
+def top_ten(category,Your_Team):
+	# You have to chhose category - capital letters, and Team name in letter case
+	# options are : ASSITS, BLOCKS, POINTS, STEALS, REBOUNDS ,
+	# DEFENSIVE_REBOUNDS, OFFENSIVE_REBOUNDS , FREE_THROW_PERCENTAGE,
+	# THREE_POINT_PERCENTAGE, FIELD_GOAL_PERCENTAGE
+	
+	team = roster(Your_Team)
+	if category=="REBOUNDS":
+		category = "TOTAL_REBOUNDS"
+	source = requests.get('https://sports.yahoo.com/nba/stats/individual/?sortStatId='+category+'_PER_GAME&selectedTable=0/').text
 
-stats = BeautifulSoup(source,'lxml')
+	stats = BeautifulSoup(source,'lxml')
+	players = stats.find_all('tr', attrs={'class':'Bgc(table-hover):h'})
+	i = 1
+	export_lst = []
+	for player in players:
+			player_name=player.th.div.a.text
+			if (player_name not in team):
+				export_lst.append(player_name)
+			i += 1
+			if i==21 or len(export_lst)==10:
+				break
+	print(export_lst)
 
-players = stats.find_all('tr', attrs={'class':'Bgc(table-hover):h'})
-for player in players:
-		player_name=player.th.div.a.text
-		print(player_name)
+
--- team_roster.py
+++ team_roster.py
@@ -5,7 +5,7 @@ def roster(teamname):
     url = "http://data.nba.net/json/cms/noseason/team/"+(teamname)+"/roster.json"
 
     response = requests.request("GET", url)
-
+    team = []
     data = json.loads(response.text)
     try:
         data2 = data["sports_content"]["roster"]['players']['player']
@@ -13,9 +13,9 @@ def roster(teamname):
             for d in data2:
                 first_name = d['first_name']
                 last_name = d['last_name']
-                print(first_name, last_name)
+                team.append(first_name +" "+ last_name)
+            return (team)
     except :
         print("No such a team")
 
 
-roster("lakers")
